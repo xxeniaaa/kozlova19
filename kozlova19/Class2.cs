2da4501f38ce289ad540ca8f7198ef381e959077
xeanko@yandex.ru <xeanko@yandex.ru>
2023-07-05 09:46:41 +0300

    Создание 2 классов: Team и Player

---
+++ kozlova19/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kozlova19
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Player(string nm, string dscp)
+        {
+            Name = nm;
+            Description = dscp;
+        }
+        public void Show()
+        {
+            Console.WriteLine(
+                 $"Название: {Name}, Описание - {Description}");
+        }
+    }
+}
